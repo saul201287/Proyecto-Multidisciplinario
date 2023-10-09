dd9615657f9352fc8e3870f8131aa1e846de1003
saul201287 <jssaul99@gmail.com>
2023-10-09 06:11:24 -0600

    Changes

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,15 +1,13 @@
 import { Outlet, useLocation } from "react-router-dom";
-
+import "../styles/MenuAdministrador.css";
+import Nav from "../components/Nav";
 function Layout() {
   const location = useLocation();
 
-  
   return (
     <body>
-     
-       <Outlet />
-
-
+      <Nav />
+      <Outlet />
     </body>
   );
 }
